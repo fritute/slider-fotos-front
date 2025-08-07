0742d10672ec1223a500cd17ea3d4c5f68e18c73
GUSTAVO ZUMBA DE AMORIM <24124573@dsn127m1242376.cfp127aluno.local>
2025-08-07 11:02:00 -0300

    url atualizada

--- src/assets/components/ImageSlider.jsx
+++ src/assets/components/ImageSlider.jsx
@@ -10,7 +10,7 @@ const ImageSlider = () => {
   useEffect(() => {
     const fetchFotos = async () => {
       try {
-        const response = await fetch('http://localhost:3000/fotos');
+        const response = await fetch('https://slider-fotos-back-senai.onrender.com/fotos');
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
